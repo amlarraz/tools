6b3f95c423f7b80823edf15d17bad2587e5afba4
amlarraz <amlarraz@users.noreply.github.com>
2017-03-15 17:26:25 +0100

    Work during the training not only when finish
    Added some lines to can use the script during the training, not only when the training finish

--- plot_learning_curve.py
+++ plot_learning_curve.py
@@ -16,6 +16,7 @@ from os import getcwd, listdir
 import numpy as np
 import matplotlib.pyplot as plt
 import subprocess
+import os
 
 wd = getcwd()
 
@@ -29,14 +30,34 @@ log_file = open(wd+'/experiments/logs/'+name_log,'r')
 
 #Clean the log file, remove all the code we dont need:
 
-lines_from_log = log_file.readlines()
+lines_from_log = log_file.readlines() #Read the log
+
+num_iters=0 #Number of iterations for the training
+first_row = 0 #First log line we need
+last_row = 0 #Last log line we need
+dataset_name=''
 
 for i in range(len(lines_from_log)):
+
+	if str.find(lines_from_log[i],'max_iters=')!=-1:
+		n1 = lines_from_log[i].find('max_iters=')
+		n2 = lines_from_log[i].find(', pretrained_model')
+		num_iters = int(lines_from_log[i][n1:n2].replace('max_iters=',''))
+		n1 = lines_from_log[i].find('imdb_name=')
+		n2 = lines_from_log[i].find(', max_iters')
+		dataset_name = lines_from_log[i][n1:n2].replace('imdb_name=','')[1:len(lines_from_log[i][n1:n2].replace('imdb_name=',''))-1]
+		
 	if str.find(lines_from_log[i],'Iteration 0, loss')!=-1:
 		first_row = i #first row that we need
-	if str.find(lines_from_log[i],'Wrote snapshot to:')!=-1:
+
+	if str.find(lines_from_log[i],'Iteration '+str(num_iters - 20)+', lr')!=-1:
 		last_row = i #last row that we need
-	
+
+if last_row == 0:
+	last_row = len(lines_from_log) #If the training hasnt finished
+
+#Search the dataset name:
+
 lines_we_need = lines_from_log[first_row:last_row]
 
 output = open(wd+'/prueba_output.txt','w')
@@ -45,33 +66,36 @@ for i in range(len(lines_we_need)):
 	if str.find(lines_we_need[i],'speed:')==-1:
 		output.write(lines_we_need[i])
 
-#Extract mAP and AP for each class:
+#Extract mAP and AP for each class: (Only if the training has finished)
 
-APs = []
-class_names = []
+if last_row != len(lines_from_log): #Check if the training has finished
 
-for i in range(len(lines_from_log)):
-	if str.find(lines_from_log[i],'VOC07 metric? Yes')!=-1:
-		first_row = i+1 #first row that we need
-	if str.find(lines_from_log[i],'Mean AP')!=-1:
-		last_row = i+1 #last row that we need
+	APs = []
+	class_names = []
 
-aux_ap = lines_from_log[first_row :last_row]
+	for i in range(len(lines_from_log)):
+		if str.find(lines_from_log[i],'AP for')!=-1:
+			first_row_ap = i #first row that we need
+			
+		if str.find(lines_from_log[i],'Mean AP')!=-1:
+			last_row_ap = i+1 #last row that we need
 
+	aux_ap = lines_from_log[first_row_ap :last_row_ap]
+	
+	for i in range(len(aux_ap)-1):
+		APs.append(float(aux_ap[i].replace('AP for ','').split('=')[1].strip()))
+		class_names.append(aux_ap[i].replace('AP for ','').split('=')[0].strip())
 
-for i in range(len(aux_ap)-1):
-	APs.append(float(aux_ap[i].replace('AP for ','').split('=')[1].strip()))
-	class_names.append(aux_ap[i].replace('AP for ','').split('=')[0].strip())
+	APs.append(float(aux_ap[len(aux_ap)-1].split('=')[1].strip()))
 
-APs.append(float(aux_ap[len(aux_ap)-1].split('=')[1].strip()))
-
-class_names.append(aux_ap[len(aux_ap)-1].split('=')[0].strip())
+	class_names.append(aux_ap[len(aux_ap)-1].split('=')[0].strip())
 
 output.close()
 
 output=open('prueba_output.txt','rw')
 
 lines_output =output.readlines()
+
 lines=[]
 iterations=[]
 loss=[]
@@ -113,24 +137,34 @@ plt.ylabel("Loss")
 #Graph for learning rate:
 
 plt.subplot(2,2,3)
-plt.plot(iterations,lr,'g',linewidth=2, label='lr')
+
+#Necessary "if"...':)
+
+if last_row != len(lines_from_log):
+	plt.plot(iterations[:len(iterations)-1],lr,'g',linewidth=2, label='lr')
+else:
+	plt.plot(iterations,lr,'g',linewidth=2, label='lr')
+
 plt.legend()
 plt.title("Learning Rate",fontsize=18)
 plt.xlabel("Iteration")
 plt.ylabel("Learning Rate")
 plt.ylim(0,0.002)
 
-#Graph for APs:
-
-plt.subplot(2,2,4)
-plt.bar(range(len(class_names)-1),APs[:len(APs)-1],color ='#ff9999',align='center')
-plt.title("mAP: "+str(APs[len(APs)-1]),fontsize=18)
-plt.xticks(np.arange(len(class_names)-1),class_names[:len(class_names)-1],rotation = 90)
-plt.xlim(-1,len(class_names)-1)
+#Graph for APs:(only if the training finish)
+if last_row != len(lines_from_log):
+	plt.subplot(2,2,4)
+	plt.bar(range(len(class_names)-1),APs[:len(APs)-1],color ='#ff9999',align='center')
+	plt.title("mAP: "+str(APs[len(APs)-1]),fontsize=18)
+	plt.xticks(np.arange(len(class_names)-1),class_names[:len(class_names)-1],rotation = 90)
+	plt.xlim(-1,len(class_names)-1)
 
 #Save the graph:
 
-plt.savefig('learning_curve_'+str(max(iterations)))
+if not os.path.exists(wd+'/models/'+dataset_name.split('_')[0]+'/curves'):
+	os.mkdir(wd+'/models/'+dataset_name.split('_')[0]+'/curves')
+
+plt.savefig(wd+'/models/'+dataset_name.split('_')[0]+'/curves/'+'learning_curve_'+str(max(iterations)))
 
 #To show the graphs:
 
