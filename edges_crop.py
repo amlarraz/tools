97cd9e83d55b07ef2c0456483a97034dcf61302f
amlarraz <amlarraz@users.noreply.github.com>
2017-04-18 20:28:54 +0200

    Improve the code

--- edges_crop.py
+++ edges_crop.py
@@ -12,7 +12,7 @@ INFER_FASTER = '/home/poto/py-faster-rcnn/tools/infer_grapes.py'
 WEIGHTS_FASTER = '/home/poto/py-faster-rcnn/output/faster_rcnn_end2end/grapes_trainval/vgg16_faster_rcnn_iter_70000.caffemodel'
 
 INFER_DEEPLABV2 = '/home/poto/Escritorio/deeplabv2/TF/tensorflow-deeplab-resnet/inference.py'
-WEIGHTS_DEEPLABV2 = '/home/poto/Escritorio/deeplabv2/TF/tensorflow-deeplab-resnet/models/grapes/snapshots_finetune/model.ckpt-20000'
+WEIGHTS_DEEPLABV2 = '/home/poto/Escritorio/deeplabv2/TF/tensorflow-deeplab-resnet/models/grapescrop/snapshots_finetune/model.ckpt-20000'
 
 SAVE_DIR = '/home/poto/Escritorio/DATASETS/GRAPES/final_results/'
 
@@ -48,11 +48,13 @@ def main():
     #Read test.txt:
 
     args = get_arguments(TEST_FILE,IMG_DIR,SAVE_DIR)
-    test_file = open(args.test_file, 'r')
-    test_list = test_file.readlines()
 
-    #Create the final_results dir:
+    with open(args.test_file, 'r') as f:
 
+    	test_list = [l.strip() for l in f.readlines()]
+    	
+    #Create the final_results dir: ASK TO IVAN ABOUT HIS COMMENT HERE
+    
     if not os.path.exists(args.save_dir):
 		os.mkdir(args.save_dir)
 		os.mkdir(args.save_dir+'results_faster')
@@ -61,44 +63,43 @@ def main():
 
     ################# Inference from Faster-RCNN ####################################
 
-    os.system('python '+args.infer_faster+' --net '+args.weights_faster+' --imgfolder '+args.img_dir+' --outfolder '+args.save_dir+'results_faster --imgsetfile '+args.test_file)
+    #os.system('python '+args.infer_faster+' --net '+args.weights_faster+' --imgfolder '+args.img_dir+' --outfolder '+args.save_dir+'results_faster --imgsetfile '+args.test_file)
 
     ################# Crop the images to obtain only the areas with detection #######    
 
-    bboxes_file = open(args.save_dir+'results_faster/bboxes.txt', 'r')
-    bboxes_list = bboxes_file.readlines()
-
+    with open(args.save_dir+'results_faster/bboxes.txt', 'r') as f:
+    
+    	bboxes_list = [l.strip() for l in f.readlines()]
+    
     for i in range(len(test_list)):
     	for j in range(len(bboxes_list)):
-    		if bboxes_list[j][:bboxes_list[j].find(' ')].replace('.jpg','') == test_list[i].replace('\n',''):
+    		if bboxes_list[j][:bboxes_list[j].find(' ')].replace('.jpg','') == test_list[i]:
 
     			x_min = int(bboxes_list[j].split(' ')[2].split('.')[0])
     			y_min = int(bboxes_list[j].split(' ')[3].split('.')[0])
     			x_max = int(bboxes_list[j].split(' ')[4].split('.')[0])
     			y_max = int(bboxes_list[j].split(' ')[5].split('.')[0])
 
-    			full_img = cv2.imread(args.img_dir+test_list[i].replace('\n','')+'.jpg')
+    			full_img = cv2.imread(args.img_dir+test_list[i]+'.jpg')
     			crop = full_img[y_min:y_max,x_min:x_max ,:]
-    			cv2.imwrite(args.save_dir+'crops/'+test_list[i].replace('\n','')+'_'+str(j)+'.jpg',crop)
+    			cv2.imwrite(args.save_dir+'crops/'+test_list[i]+'_'+str(j)+'.jpg',crop)
 
     ################# Inference from DeepLabV2 ######################################
 
-    lista = os.walk(args.save_dir+'crops')
-
-    for root, dirs, files in lista:
-    	for i in range(len(files)):
-    		os.system('python '+args.infer_deeplabv2+' '+args.save_dir+'crops/'+files[i]+' '+args.weights_deeplabv2+' --save-dir '+args.save_dir+'results_deeplabv2/')
+    crops = os.listdir(args.save_dir+'crops')
+    
+    #for i in range(len(crops)):
+    #	os.system('python '+args.infer_deeplabv2+' '+args.save_dir+'crops/'+crops[i]+' '+args.weights_deeplabv2+' --save-dir '+args.save_dir+'results_deeplabv2/')
 
     ################# Overlap image and segmentation ################################
 
     for i in range(len(test_list)):
 	
-	full_img = cv2.imread(args.img_dir+test_list[i].replace('\n','')+'.jpg')
-	crops = os.listdir(args.save_dir+'crops')
-    		
+	full_img = cv2.imread(args.img_dir+test_list[i]+'.jpg')
+	    		
 	for k in range(len(crops)):
 		
-		if crops[k][:crops[k].find('_')]==test_list[i].replace('\n',''):
+		if crops[k][:crops[k].find('_')]==test_list[i]:
 
 			#Open images:
 	
@@ -109,22 +110,17 @@ def main():
 	
 	    		laplacian = cv2.Laplacian(mask, cv2.CV_64F, ksize = 5)
 
-	    		#Save the edges image:
+	    		#Change the color of the edges:
+			
+       			is_border = laplacian[:,:,2]!=0
+			is_mask = mask[:,:,2]==255
+			
+			#Overlap the images:
 
-	    		cv2.imwrite(args.save_dir+'results_deeplabv2/Laplacian.png', laplacian)
+			output_edge=orig.copy()
+			output_edge[is_border] = [255,255,0]
 
-    			laplacian=cv2.imread(args.save_dir+'results_deeplabv2/Laplacian.png')
-
-       			#Change the color of the edges:
-
-       			laplacian[laplacian[:,:,2]==255] = [255,255,0]
-
-       			#Overlap images: (output_edge = original+edge, output_mask = orig+mask)
-
-       			output_edge=cv2.addWeighted(orig,1,laplacian,1,0)
-       			output_mask=cv2.addWeighted(orig,1,mask,0.6,0)
-		
-			#Paste the crops+edge in the original image:
+			#Draw bboxes:
 
 			for j in range(len(bboxes_list)):
 				if crops[k] == bboxes_list[j][:bboxes_list[j].find(' ')].replace('.jpg','_'+str(j)+'.jpg'):
@@ -133,19 +129,15 @@ def main():
 					y_min = int(bboxes_list[j].split(' ')[3].split('.')[0])
 					x_max = int(bboxes_list[j].split(' ')[4].split('.')[0])
 					y_max = int(bboxes_list[j].split(' ')[5].split('.')[0])
-
 				
 					full_img[y_min:y_max,x_min:x_max ,:] = output_edge
 					cv2.rectangle(full_img,(x_min,y_min),(x_max,y_max),(6,24,207),3)
     	
 			cv2.imwrite(args.save_dir+crops[k][:crops[k].find('_')]+'_edges.jpg',full_img)	
 					
-			# Remove the Edge image:
-
-       			os.system('rm '+args.save_dir+'results_deeplabv2/Laplacian.png')
-
+			
     print ''
-    print 'All is done'
+    print ('All is done')
     print ''
 
 if __name__ == "__main__":
